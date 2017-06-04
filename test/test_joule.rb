9ea166ca207429165b9b2cc337d047d6bcf69339
ItsJonQ <itsjonq@gmail.com>
2017-06-03 22:45:46 -0400

    Add additional tests

--- test/test_joule.rb
+++ test/test_joule.rb
@@ -14,4 +14,57 @@ class JouleTest < JekyllUnitTest
     assert(el.text.include?("Hello"))
     assert(el["class"].include?("hello"))
   end
+
+  should "render an include html file" do
+    html = %Q[
+      <div class="hello">
+        {% include test.html %}
+        Hello
+      </div>
+    ]
+    @joule.render(html)
+    el = @joule.css(".test")[0]
+
+    assert(el)
+    assert(el["class"].include?("test"))
+  end
+
+  class JouleFindTest < JekyllUnitTest
+    should "get single node element" do
+      html = %Q[
+        <div class="hello">
+          Hello
+        </div>
+      ]
+      @joule.render(html)
+      el = @joule.find(".hello")
+
+      assert(el)
+      assert(el["class"].include?("hello"))
+      assert(el.text.include?("Hello"))
+    end
+  end
+
+  class JouleFindAllTest < JekyllUnitTest
+    should "get all node elements" do
+      html = %Q[
+        <div class="hello">
+          Hello
+        </div>
+        <div class="hello">
+          There
+        </div>
+      ]
+      @joule.render(html)
+      els = @joule.find_all(".hello")
+
+      assert(els)
+      assert_equal(els.length, 2)
+
+      el = els[0]
+      assert(el)
+      assert(el["class"].include?("hello"))
+      assert(el.text.include?("Hello"))
+    end
+  end
 end
